199706967b501a05e7a7a767e02541d6cdcb828d
Nana_77 <nana77amery@gmail.com>
2021-10-23 16:13:45 +0900

    キャラクターの追加と新しいゲームシーンの作成

---
+++ JampJampJamp！/Assets/Script/Click.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Click : MonoBehaviour
+{    
+    public void Rule()
+    {
+        SceneManager.LoadScene("Rule");
+    }
+
+    public void Game()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
